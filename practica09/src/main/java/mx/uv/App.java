1a7019e1a7723bbb67248ceeb9718ee2689aa740
JorgeCasas01 <jorgeluiscazas@gmail.com>
2023-10-19 12:55:05 -0600

    practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -1,7 +1,8 @@
 package mx.uv;
 
+import static spark.Spark.*;
 /**
- * Hello world!
+ * Salut!
  *
  */
 public class App 
@@ -9,5 +10,18 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        get("/",
+            (request,response)->"<h1>hola mundo</h1>"
+        );
+        get("/ruta1",
+            (request,response)->"<h1>adios mundo</h1>"
+        );
+        get("/ruta2",
+            (request,response)->"<h1>hola oaxaca</h1>"
+        );
+        get("/ruta3",
+            (request,response)->"{'alumno':'john','carrera':'tc','semestre':'6'}"
+        );
     }
 }
